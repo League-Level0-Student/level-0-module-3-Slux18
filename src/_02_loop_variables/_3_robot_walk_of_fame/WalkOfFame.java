4cbc6653c98b3c5d2f2bd6c22796ced49ce0d210
league <richardson.liou@gmail.com>
2023-02-06 18:30:32 -0800

    im almost done with module 3

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -6,25 +6,89 @@ import org.jointheleague.graphical.robot.Robot;
 public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
-		rob.setSpeed(50);
+		rob.setSpeed(10000000);
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
 		rob.turn(-90);
 rob.move(350);
 rob.turn(90);
-rob.turn(180);
-rob.move(175);
-rob.turn(180);
-		// 2. Make the robot draw a star shape. Hint: angle=144.
+// 2. Make the robot draw a star shape. Hint: angle=144.
+	// 3. Set the length of each line in the star to 30.
+int starTracker=0;
+while(starTracker<=9) {
+	
 
-		// 3. Set the length of each line in the star to 30.
+rob.penDown();
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(90);
+rob.penUp();
+rob.move(50);
+rob.turn(-90);
 
+starTracker+=1;
+}
+rob.penUp();
+rob.turn(90);
+rob.move(300);
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of 10 stars (see recipe to see how it should look)
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
+//rob.penUp();
+//rob.turn(90);
+//rob.move(50);
+//rob.penDown();
+//rob.turn(144);
+//rob.move(30);
+//rob.turn(144);
+//rob.move(30);
+//rob.turn(144);
+//rob.move(30);
+//rob.turn(144);
+//rob.move(30);
+//rob.turn(144);
+//rob.move(30);
+//rob.penUp();
+//rob.turn(90);
+//rob.move(50);
+//rob.penDown();
+//rob.turn(144);
+//rob.move(30);
+//rob.turn(144);
+//rob.move(30);
+//rob.turn(144);
+//rob.move(30);
+//rob.turn(144);
+//rob.move(30);
+//rob.turn(144);
+//rob.move(30);
+//rob.penUp();
+//rob.turn(90);
+//rob.move(50);
+//rob.penDown();
+//rob.turn(144);
+//rob.move(30);
+//rob.turn(144);
+//rob.move(30);
+//rob.turn(144);
+//rob.move(30);
+//rob.turn(144);
+//rob.move(30);
+//rob.turn(144);
+//rob.move(30);
+
+
 
 	}
 
